dbd7187c15fcda368652e5c79e582bdaf01a8222
j-palacio <jaypalacio95.com>
2023-12-23 07:05:01 -0500

    Merged file and commited changes I believe

--- Lottery Number Generator.py
+++ Lottery Number Generator.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Initialize list of numbers.
 numbers = []
 
@@ -12,3 +13,19 @@ for number in numbers:
 # Separate current number from next number.
     print(number, end=' ')
 print()
+=======
+# Initialize list of numbers.
+numbers = []
+
+# Assign random numbers to list.
+import random
+
+for i in range(7):
+    numbers.append(random.randint(0, 9))
+
+# Display numbers in a single line.
+for number in numbers:
+# Separate current number from next number.
+    print(number, end=' ')
+print()
+>>>>>>> c8d26da734438eb485816b03ad23dba66576f2e4
